e9e754cd967602a0d292cd99d1f4824a86f0aabc
karam  mustafa <karam2mustafa@gmail.com>
2022-04-20 18:29:20 +0300

    add contribution section

---
+++ resources/views/components/contribution-section.blade.php
@@ -0,0 +1,14 @@
+<div class="contribution-section container mt-5">
+    <div class="row w-100 h-100">
+        <div class="col-md-6">
+            <img src="{{asset('images/open-source.png')}}">
+        </div>
+        <div class="col-md-6 mt-5">
+            <div class="event-introduction text-center w-100">
+                <h1 class="font-weight-bold"> {{trans('global.titles.contributions_program')}} </h1>
+                <p class="intro-p"> {{trans('global.texts.contributions_program_text')}}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
