663c0fbfb257594f109216f74e8d269777b16af9
karam  mustafa <karam2mustafa@gmail.com>
2022-04-21 16:30:12 +0300

    refactor and improve

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\SendEmail;
-use App\Http\Requests\RegisterRequest;
-use App\Mail\SendRegisteredEmail;
-use App\Models\Activity;
-use App\Models\Hotel;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -21,57 +13,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $hotels = Hotel::all();
-        $activities = Activity::all();
-
-        return view('pages.home', compact('hotels', 'activities'));
-    }
-
-    /**
-     * load the main web page.
-     *
-     * @param  \App\Http\Requests\RegisterRequest  $request
-     *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     * @author karam mustafa
-     */
-    public function register(RegisterRequest $request)
-    {
-
-        DB::transaction(function () use ($request) {
-            $data = $request->all();
-            $data['password'] = bcrypt($request->password);
-
-            $data = $this->calculateTotalCost($request, $data);
-
-            $user = User::create($data);
-
-            event(new SendEmail($user));
-        });
-
-        return ;
-    }
-
-    /**
-     * description
-     *
-     * @param  \App\Http\Requests\RegisterRequest  $request
-     * @param  array  $data
-     *
-     * @return array
-     * @author karam mustafa
-     */
-    private function calculateTotalCost(RegisterRequest $request, array $data)
-    {
-        $data['total_cost'] = 0;
-
-        if ($request->activities) {
-            $data['total_cost'] += Activity::whereIn('id', $request->activities)->sum('price');
-        }
-
-        if ($request->hotel_id) {
-            $data['total_cost'] += Hotel::where('id', $request->hotel_id)->first()->price;
-        }
-        return $data;
+        return view('pages.home');
     }
 }
