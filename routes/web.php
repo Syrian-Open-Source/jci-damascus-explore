6349bcaefe07de67ae49d43f03172b4880c9025b
Yahia bajbouj <you@example.com>
2022-05-06 14:13:15 +0300

    read qr code

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QrCodeController;
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
