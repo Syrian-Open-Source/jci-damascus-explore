b32feb1196ceb622eba89e26715fce86781ae12e
karam  mustafa <karam2mustafa@gmail.com>
2022-05-18 21:14:35 +0300

    wip

---
+++ resources/views/pages/register_finish.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container my-5">
+        @include('layouts.sessions')
+        <h1 class="f-red font-weight-bold">{{trans('global.closed_website_message')}}</h1>
+        <p>{{trans('global.texts.introduction')}}</p>
+    </div>
+@endsection
