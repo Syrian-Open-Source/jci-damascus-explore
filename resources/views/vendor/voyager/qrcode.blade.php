5fb293442ca9539dde0a35b105401a718b39d92b
karam  mustafa <karam2mustafa@gmail.com>
2022-05-20 23:41:49 +0300

    wip

--- resources/views/vendor/voyager/qrcode.blade.php
+++ resources/views/vendor/voyager/qrcode.blade.php
@@ -48,12 +48,17 @@
     <script>
         var resultContainer = document.getElementById('qr-reader-results');
         var lastResult, countResults = 0;
+
         function onScanSuccess(decodedText, decodedResult) {
             if (decodedText !== lastResult) {
                 ++countResults;
                 lastResult = decodedText;
+
+                let chooseActivity = document.querySelector('.activity');
+                console.log(chooseActivity)
             }
         }
+
         var html5QrcodeScanner = new Html5QrcodeScanner(
             "qr-reader", {fps: 10, qrbox: 250});
         html5QrcodeScanner.render(onScanSuccess);
