edf9be0a49859584724ea9b3d9e20e330d4d8b36
karam  mustafa <karam2mustafa@gmail.com>
2022-05-24 12:05:19 +0300

    wip

--- resources/views/file/identification.blade.php
+++ resources/views/file/identification.blade.php
@@ -27,14 +27,14 @@
   <div style="position: relative; width: 100%; height: 100%; margin: auto;">
     <img src="https://explore.jci-damascus.com/images/qrBackground.png" style="position: absolute; width: 100%; height: 100%; z-index: -1000; border:5px solid #000" />
     <div style="position: absolute; width: 100%; height: 100%; text-align: center; color: white; line-height: 1.5">
-      <span style="color: black; padding: .5em; margin-top: 70px; display:block; font-size:15px; font-weight: bold;" class="text-center">{{$user->fill_name_en}}</span> <br />
+      <span style="color: black; padding: .5em; margin-top: 120px; display:block; font-size:30px; font-weight: bold;" class="text-center">{{$user->fill_name_en}}</span> <br />
       <span style="color: black; padding: .5em;" class="text-center">{{$user->local_room}}</span>
     </div>
   </div>
   <div class="page-break"></div>
   <div style="position: relative; width: 100%; height: 100%; margin: auto;">
     <img src="https://explore.jci-damascus.com/images/qrBackground.png" style="position: absolute; width: 100%; height: 100%; z-index: -1000; border:5px solid #000" />
-    <div style="position: absolute; left: 10%; top: -10%; text-align: center; color: white; line-height: 1.5">
+    <div style="position: absolute; left: 30%; top: 20%; text-align: center; color: white; line-height: 1.5">
       <img class="img-center" src="data:image/png;base64, {{ $qrCode }}" />
     </div>
   </div>
